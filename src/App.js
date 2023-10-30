d8490c9b2e613125c3d32459d6d4ae5f81283209
Melissa Mackie <melissa.mackie@live.com>
2023-10-29 18:22:01 -0700

    testing and setting up intitial files

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Home from './components/home/home';
+// import About from './components/about/about';
+// import Projects from './components/projects/projects';
+// import Nav from './components/nav/nav';
+// import Contacts from './components/contact/contact';
+// import Footer from './components/footer/footer';
+
+function App() {
+  return (
+    <>
+     <Home />
+     {/* <Nav />
+     <About />
+     <Projects />
+     <Contacts />
+     <Footer /> */}
+    </>
+  )
+}
+
+export default App;
