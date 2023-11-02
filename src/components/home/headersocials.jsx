2ddbfda321ae1d3dbee266c1dd65449ed8b8555e
Melissa Mackie <melissa.mackie@live.com>
2023-11-02 10:51:31 -0700

    home setup

---
+++ src/components/home/headersocials.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaFreeCodeCamp } from "react-icons/fa";
+import {AiOutlineTwitter} from "react-icons/ai"
+
+ 
+
+const HeaderSocials = () => {
+  return (
+    <div className='header__socials'>
+        <a href="https://www.linkedin.com/in/melissa-m-mackie" target="_blank" rel="noreferrer"><FaLinkedin /></a>
+        <a href="https://github.com/m-mackie" target="_blank" rel="noreferrer"><FaGithub /></a>
+        <a href="https://www.freecodecamp.org/wolfaholic" target="_blank" rel="noreferrer"><FaFreeCodeCamp /></a>
+        <a href="https://twitter.com/CodeMackie" target="_blank" rel="noreferrer"><AiOutlineTwitter /></a>
+    </div>
+  )
+}
+
+export default HeaderSocials
---
+++ src/components/home/home.jsx
@@ -0,0 +1,33 @@
+import './home.css';
+import CallToAction from './calltoaction/calltoaction'
+import HeaderSocials from './headersocials';
+import CyberSpace from '../../assets/img/cyberspace.png';
+ 
+
+const Home = () => {
+    return (
+         <header>
+          
+                <div className="name_container">
+                    <h1>Hello, I am <span className="name_highlight">Melissa</span>,</h1>
+                    <h3>Full-Stack Webdeveloper</h3>
+                    <p>I create beautiful things. I am a passionate creator and I beleive that life is a journey of continued learning.</p>
+                    <CallToAction />
+                </div>
+               
+                <div className="image_container">
+                    
+                    <img src={CyberSpace} alt="woman staring into a cyberspace setting" />
+                    <p className="quote">
+                        "Failure is apart of the process. You just learn to pick yourself back up." - Michelle Obama
+                    </p>
+                </div>
+                
+                <HeaderSocials />
+                <a href="#contact" className="scroll__down">Scroll Down</a>
+           
+         </header>
+    )
+}
+
+export default Home;
